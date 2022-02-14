a0f843e6082bc3906660ba9252905ca42d8b810c
zygimantus <zygimantus@gmail.com>
2022-02-14 23:10:21 +0200

    Removing console printing

--- src/main/java/org/zygimantus/chess/Game.java
+++ src/main/java/org/zygimantus/chess/Game.java
@@ -2,7 +2,6 @@ package org.zygimantus.chess;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.zygimantus.chess.pieces.ChessPiece;
 
 @SpringBootApplication
 public class Game {
@@ -10,31 +9,5 @@ public class Game {
     public static void main(String[] args) {
 
         SpringApplication.run(Game.class, args);
-
-        System.out.println("Preparing a chess game!");
-
-        Equipment equipment = new Equipment();
-        Board board = equipment.getBoard();
-        draw(board);
-
-        System.out.println("Exiting!");
-    }
-
-    public static void draw(Board board) {
-
-        ChessPiece[][] squares = board.getSquares();
-
-        StringBuilder stringBuilder = new StringBuilder();
-        for (ChessPiece[] rank : squares) {
-            for (ChessPiece chessPiece : rank) {
-                if (chessPiece == null) {
-                    stringBuilder.append(" ");
-                } else {
-                    stringBuilder.append(chessPiece.getSymbol());
-                }
-            }
-            stringBuilder.append("\n");
-        }
-        System.out.println(stringBuilder);
     }
 }

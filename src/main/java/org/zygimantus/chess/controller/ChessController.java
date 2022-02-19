544eb5d24a7a79bc9c74484abce47bdcba43209c
zygimantus <zygimantus@gmail.com>
2022-02-19 16:31:19 +0200

    Added reset button

--- src/main/java/org/zygimantus/chess/controller/ChessController.java
+++ src/main/java/org/zygimantus/chess/controller/ChessController.java
@@ -1,10 +1,9 @@
 package org.zygimantus.chess.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.zygimantus.chess.board.TwoPlayerBoard;
 import org.zygimantus.chess.pieces.ChessPiece;
 import org.zygimantus.chess.service.BoardInitializer;
@@ -14,21 +13,28 @@ public class ChessController {
 
     private final BoardInitializer boardInitializer;
 
-    @Autowired
     public ChessController(BoardInitializer boardInitializer) {
         this.boardInitializer = boardInitializer;
     }
 
     @GetMapping("/board")
-    public String board(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
-        model.addAttribute("name", name);
-
+    public String board(Model model) {
         TwoPlayerBoard twoPlayerBoard = boardInitializer.prepareInitialBoardSetup();
         ChessPiece[][] squares = twoPlayerBoard.getSquares();
 
         model.addAttribute("squares", squares);
 
         return "board";
+    }
+
+    @GetMapping("/reset")
+    public String reset(ModelMap map) {
+        TwoPlayerBoard twoPlayerBoard = boardInitializer.prepareInitialBoardSetup();
+        ChessPiece[][] squares = twoPlayerBoard.getSquares();
+
+        map.addAttribute("squares", squares);
+
+        return "board :: #board";
     }
 
 }
